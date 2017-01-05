6a632b6da91cd1750672229799e742a83d537df2
John McCutchan <john.mccutchan.wd@gmail.com>
2017-01-05 13:25:01 -0600

    initial

--- myJavascript.js
+++ myJavascript.js
@@ -1,36 +1,31 @@
 //sample data modified from randomUser.me
-var randomUser = {
-  "results": [
-    {
-      "gender": "female",
-      "name": {
-        "title": "ms",
-        "first": "stella",
-        "last": "meyer"
-      },
-      "location": {
-        "street": "7385 kapellenweg",
-        "city": "schmalkalden-meiningen",
-        "state": "saarland",
-        "postcode": 58225
-      },
-      "email": "stella.meyer@example.com",
-      "picture": {
-        "medium": "https:\/\/randomuser.me\/api\/portraits\/med\/women\/50.jpg"
-      },
-      "nat": "DE"
-    }
-  ]
+
+
+var randomGrab;
+function getRandom(){
+  return new Promise(function(resolve, reject){
+    $.ajax({
+      url: `https://randomuser.me/api/`
+    })
+    .done(function(data, textStatus, XHR){
+      resolve(data);
+      console.log(data);
+    })
+  })
 }
-
+getRandom()
+.then(function(value){
 //handlebars step one: grap the html from the script tag
-
+var entryHTML = $("#random-user").html();
 
 //handlebars step two: compile it into a template
-
+var entryTemplate = Handlebars.compile(entryHTML);
 
 //handlebars step three:render the HTML by passing the data to the template
-
+var entryOutput = entryTemplate(value);
+console.log(entryOutput);
 
 //handlebars step four: put the complete HTML into the DOM
+$("#output").append(entryOutput);
 
+})
